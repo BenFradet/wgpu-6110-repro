226861d534800a223d081a3bd873cf8dacd6c04e
benfradet <benjamin.fradet@gmail.com>
2025-10-10 19:02:37 +0200

    fn to get device and q

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use wgpu::{
+    Device, DeviceDescriptor, ExperimentalFeatures, Features, Instance, Limits, MemoryHints, Queue,
+    Trace,
+};
+
+const SHADER: &'static str = include_str!("shader.wgsl");
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let (device, queue) = get_device_and_queue();
+}
+
+async fn get_device_and_queue_async() -> (Device, Queue) {
+    let instance = Instance::default();
+    let adapter = wgpu::util::initialize_adapter_from_env(&instance, None)
+        .expect("No suitable GPU adapters found on the system");
+    let info = adapter.get_info();
+    println!(
+        "Using {:#?} {} with {:#?} backend",
+        info.device_type, info.name, info.backend
+    );
+    let downlevel_capabilities = adapter.get_downlevel_capabilities();
+    if !downlevel_capabilities
+        .flags
+        .contains(wgpu::DownlevelFlags::COMPUTE_SHADERS)
+    {
+        panic!("Adapter does not support compute shaders");
+    }
+    let device_and_queue = adapter
+        .request_device(&DeviceDescriptor {
+            label: None,
+            required_features: Features::empty(),
+            required_limits: Limits::downlevel_defaults(),
+            memory_hints: MemoryHints::Performance,
+            trace: Trace::Off,
+            experimental_features: ExperimentalFeatures::disabled(),
+        })
+        .await
+        .unwrap();
+    device_and_queue
+}
+
+fn get_device_and_queue() -> (Device, Queue) {
+    futures::executor::block_on(get_device_and_queue_async())
 }
